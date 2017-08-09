a97d14c7d8d634affc4d18b1c8dfd09b3520c508
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-08-09 15:01:18 +0200

    details

---
+++ src/components/video_details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoDetail = (props) => {
+    return (
+        <div className="video-detail col-md-8">
+            <div className="embed-responsive embed-reponsive-16by9">
+                <iframe className="embed-responsive-item"></iframe>
+            </div>
+        </div>
+    );
+};
+
+export default VideoDetail;
